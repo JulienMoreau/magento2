ebd59c0a0000298cb0db7a964b800947aa90fde9
Julien Moreau@ <julien.moreau@adfab.fr>
2016-12-07 15:04:25 +0100

    TD 9

---
+++ _extra/scripts/testModules/_tools/params.sample.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Parameter to uses for Soap and Rest call.
+ * Go in the Magento BackOffice to generate a soap token (in system > integration)
+ *
+ * @author    Laurent Minguet <lamin@smile.fr>
+ * @copyright 2016 Smile
+ */
+$params = [
+    'main_url' => 'http://magento2.lxc/',
+    'rest_account' => [
+        'username' => 'admin',
+        'password' => 'Lveen2016',
+    ],
+    'soap_token' => '9vp3dm4mhx6q66brat8ced1dgriwtcg0',
+    'exception_on_error' => false,
+];
